cdbddb1fe6192f89b492b28a13d71b8bf5e6bb27
hayoon <hs183000@naver.com>
2022-07-15 17:51:05 +0900

    useClick

---
+++ useClick.js
@@ -0,0 +1,31 @@
+import { useState, useEffect, useRef } from "react";
+import ReactDOM from "react-dom";
+
+const useClick = (onClick) => {
+  if (typeof onClick !== "function") {
+    return;
+  }
+  const element = useRef();
+  useEffect(() => {
+    if (element.current) {
+      element.current.addEventListener("click", onClick);
+    }
+
+    return () => {
+      if (element.current) {
+        element.current.removeEventListener("click", onClick);
+      }
+    };
+  }, []);
+  return element;
+};
+
+export default function App() {
+  const sayHello = () => console.log("say hello");
+  const title = useClick(sayHello);
+  return (
+    <div className="App">
+      <h1 ref={title}>Hi</h1>
+    </div>
+  );
+}

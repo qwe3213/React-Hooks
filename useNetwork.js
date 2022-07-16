4acd4406eb0bd82b582139bd7c89f8d702b37e3f
hayoon <hs183000@naver.com>
2022-07-16 23:24:44 +0900

    useNetwork

---
+++ useNetwork.js
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+
+const useNetwork = (onChange) => {
+  const [status, setStatus] = useState(navigator.onLine);
+  const handleChange = () => {
+    if (typeof onChange === "function") {
+      onChange(navigator.onLine);
+    }
+    setStatus(navigator.onLine);
+  };
+
+  useEffect(() => {
+    window.addEventListener("online", handleChange);
+    window.addEventListener("offline", handleChange);
+    () => {
+      window.removeEventListener("online", handleChange);
+      window.removeEventListener("offline", handleChange);
+    };
+  }, []);
+  return status;
+};
+export default function App() {
+  const handleNetworkChange = (online) => {
+    console.log(online ? "we just went online" : "we are Offline");
+  };
+  const onLine = useNetwork(handleNetworkChange);
+  return (
+    <div className="App">
+      <h1>{onLine ? "Online" : "Offline"}</h1>
+    </div>
+  );

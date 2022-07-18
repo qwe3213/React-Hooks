6332995d5c3c458b4de79fdc862ec913071d0d02
hayoon <hs183000@naver.com>
2022-07-18 21:45:51 +0900

    useNotification

---
+++ useNotification.js
@@ -0,0 +1,29 @@
+
+const useNotification = (title,options)=>{
+    if(!("Notification"in window)){
+      return;
+    }
+     const fireNotif=()=>{
+      if(Notification.permission  !=="granted"){
+        Notification.requestPermission().then(permission =>{
+          if(permission==="granted"){
+              new Notification(title,options)
+          } else{
+              return;
+          }
+        })
+      } else{
+          new Notification(title,options)
+      }
+  };
+  return fireNotif
+  };
+  
+  export default function App() {
+    const triggerNotif= useNotification("can I steal your  kimch")
+    return (
+      <div className="App" style={{ height: "1000vh" }}>
+        <button onClick={triggerNotif}>Hello</button>
+      </div>
+    );
+  }
